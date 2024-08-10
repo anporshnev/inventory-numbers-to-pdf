a637198ef56fe661e177e7f0d7aa6db7f729dd09
AndrewPorshnev <andrew.porshnev@yandex.ru>
2024-08-10 12:02:52 +0300

    Добавляет экспорт номеров в pdf

--- src/main/java/com/andrewporshnev/Main.java
+++ src/main/java/com/andrewporshnev/Main.java
@@ -1,7 +1,18 @@
 package com.andrewporshnev;
 
 
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Font;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.Phrase;
+import com.itextpdf.text.pdf.BaseFont;
+import com.itextpdf.text.pdf.PdfPCell;
+import com.itextpdf.text.pdf.PdfPTable;
+import com.itextpdf.text.pdf.PdfWriter;
+
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -10,7 +21,7 @@ import java.util.List;
 public class Main {
     private static String FONT = "src/main/resources/ArialRegular.ttf";
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, DocumentException {
         if (args.length < 2) {
             throw new RuntimeException("Не указаны пути");
         }
@@ -22,7 +33,29 @@ public class Main {
             throw new RuntimeException("Укажите директорию для сохранения файла");
         }
 
-        String outputPath = file.getAbsolutePath() + "/inventory-stickers";
+        String outputPath = file.getAbsolutePath() + "/inventory-cards.pdf";
+        Document document = new Document();
+        document.setMargins(20, 20, 20, 20);
 
+        BaseFont baseFont = BaseFont.createFont(FONT, BaseFont.IDENTITY_H, BaseFont.EMBEDDED);
+        Font font = new Font(baseFont, 12, Font.NORMAL);
+        PdfWriter.getInstance(document, new FileOutputStream(outputPath));
+
+        document.open();
+
+        PdfPTable table = new PdfPTable(3);
+        table.setWidthPercentage(100);
+        dataLines.forEach(l -> addCard(table, l, font));
+
+        document.add(table);
+        document.close();
+    }
+
+    private static void addCard(PdfPTable table, String code, Font font) {
+        PdfPTable innerTable = new PdfPTable(1);
+        Phrase phrase = new Phrase("Ячейка", font);
+        innerTable.addCell(phrase);
+        innerTable.addCell("Cell2");
+        table.addCell(innerTable);
     }
 }
