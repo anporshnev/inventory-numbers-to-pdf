d0b4066be5795e68e5e373deef9ad50cde9f6e7e
AndrewPorshnev <andrew.porshnev@yandex.ru>
2024-08-12 14:13:03 +0300

    Добавляет получение данных для экспорта из excel документа

--- src/main/java/com/andrewporshnev/Main.java
+++ src/main/java/com/andrewporshnev/Main.java
@@ -10,16 +10,20 @@ import com.itextpdf.text.pdf.BaseFont;
 import com.itextpdf.text.pdf.PdfPCell;
 import com.itextpdf.text.pdf.PdfPTable;
 import com.itextpdf.text.pdf.PdfWriter;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.DataFormatter;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
 
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
-    private static String FONT = "src/main/resources/ArialRegular.ttf";
 
     public static void main(String[] args) throws IOException, DocumentException {
         if (args.length < 2) {
@@ -28,21 +32,45 @@ public class Main {
 
         File file = new File(args[1]);
         if (!file.isDirectory()) {
-            throw new RuntimeException("Укажите директорию для сохранения файла");
+            throw new RuntimeException("Не указана директория для сохранения файла");
         }
         String outputPath = file.getAbsolutePath() + "/inventory-cards.pdf";
+        List<Device> data = getData(args[0]);
 
-        List<String> dataLines = Files.readAllLines(Paths.get(args[0]));
-        exportToPdf(dataLines, outputPath);
+        if (data.isEmpty()) {
+            System.out.println("Нет данных для экспорта");
+            return;
+        }
+
+        System.out.println(data);
+//        exportToPdf(dataLines, outputPath);
 
 
 
+    }
+
+    private static List<Device> getData(String path) throws IOException {
+        List<Device> data = new ArrayList<>();
+        DataFormatter formatter = new DataFormatter();
+
+        try (Workbook workbook = WorkbookFactory.create(new File(path))) {
+            Sheet sheet = workbook.getSheetAt(0);
+            for (Row row : sheet) {
+                String[] targetRowItems = new String[2];
+                for (int i = 0; i < 2; i++) {
+                    targetRowItems[i] = formatter.formatCellValue(row.getCell(i));
+                }
+                data.add(new Device(targetRowItems[0], targetRowItems[1]));
+            }
+        }
+        return data;
     }
 
     private static void exportToPdf(List<String> data, String path) throws DocumentException, IOException {
         Document document = new Document();
         document.setMargins(20, 20, 20, 20);
 
+        String FONT = "src/main/resources/ArialRegular.ttf";
         BaseFont baseFont = BaseFont.createFont(FONT, BaseFont.IDENTITY_H, BaseFont.EMBEDDED);
         Font font = new Font(baseFont, 12, Font.NORMAL);
         PdfWriter.getInstance(document, new FileOutputStream(path));
@@ -50,12 +78,12 @@ public class Main {
         document.open();
         PdfPTable table = new PdfPTable(3);
         table.setWidthPercentage(100);
-        data.forEach(l -> addCard(table, l, font));
+        data.forEach(l -> addCell(table, l, font));
         document.add(table);
         document.close();
     }
 
-    private static void addCard(PdfPTable table, String code, Font font) {
+    private static void addCell(PdfPTable table, String code, Font font) {
         PdfPTable innerTable = new PdfPTable(1);
         Phrase phrase = new Phrase("Ячейка", font);
         innerTable.addCell(phrase);
