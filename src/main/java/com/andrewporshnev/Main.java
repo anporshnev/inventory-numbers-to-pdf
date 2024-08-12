d94d60f5883a1211ed8a16c1924c8021fbb5b3e8
AndrewPorshnev <andrew.porshnev@yandex.ru>
2024-08-12 16:55:04 +0300

    Добавляет формирование ячеек с инвентарными номерами

--- src/main/java/com/andrewporshnev/Main.java
+++ src/main/java/com/andrewporshnev/Main.java
@@ -3,14 +3,13 @@ package com.andrewporshnev;
 
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Element;
 import com.itextpdf.text.Font;
-import com.itextpdf.text.Paragraph;
 import com.itextpdf.text.Phrase;
 import com.itextpdf.text.pdf.BaseFont;
 import com.itextpdf.text.pdf.PdfPCell;
 import com.itextpdf.text.pdf.PdfPTable;
 import com.itextpdf.text.pdf.PdfWriter;
-import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.DataFormatter;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
@@ -26,27 +25,22 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) throws IOException, DocumentException {
-        if (args.length < 2) {
-            throw new RuntimeException("Не указаны пути");
-        }
+//        if (args.length < 2) {
+//            throw new RuntimeException("Не указаны пути");
+//        }
+//
+//        File file = new File(args[1]);
+//        if (!file.isDirectory()) {
+//            throw new RuntimeException("Не указана директория для сохранения файла");
+//        }
 
-        File file = new File(args[1]);
-        if (!file.isDirectory()) {
-            throw new RuntimeException("Не указана директория для сохранения файла");
-        }
-        String outputPath = file.getAbsolutePath() + "/inventory-cards.pdf";
-        List<Device> data = getData(args[0]);
-
+        List<Device> data = getData("data.xlsx");
         if (data.isEmpty()) {
-            System.out.println("Нет данных для экспорта");
-            return;
+            throw new RuntimeException("Нет данных для экспорта");
         }
 
-        System.out.println(data);
-//        exportToPdf(dataLines, outputPath);
-
-
-
+        String outputPath = "inventory-cards.pdf";
+        exportToPdf(data, outputPath);
     }
 
     private static List<Device> getData(String path) throws IOException {
@@ -66,28 +60,49 @@ public class Main {
         return data;
     }
 
-    private static void exportToPdf(List<String> data, String path) throws DocumentException, IOException {
+    private static void exportToPdf(List<Device> data, String path) throws DocumentException, IOException {
         Document document = new Document();
         document.setMargins(20, 20, 20, 20);
 
         String FONT = "src/main/resources/ArialRegular.ttf";
         BaseFont baseFont = BaseFont.createFont(FONT, BaseFont.IDENTITY_H, BaseFont.EMBEDDED);
-        Font font = new Font(baseFont, 12, Font.NORMAL);
         PdfWriter.getInstance(document, new FileOutputStream(path));
 
         document.open();
         PdfPTable table = new PdfPTable(3);
         table.setWidthPercentage(100);
-        data.forEach(l -> addCell(table, l, font));
+        data.forEach(device -> addCell(table, device, baseFont));
         document.add(table);
         document.close();
     }
 
-    private static void addCell(PdfPTable table, String code, Font font) {
+    private static void addCell(PdfPTable table, Device device, BaseFont baseFont) {
+        Font mainFont = new Font(baseFont, 14, Font.NORMAL);
+        Font descriptionFont = new Font(baseFont, 9, Font.NORMAL);
+
         PdfPTable innerTable = new PdfPTable(1);
-        Phrase phrase = new Phrase("Ячейка", font);
-        innerTable.addCell(phrase);
-        innerTable.addCell("Cell2");
-        table.addCell(innerTable);
+        PdfPCell parentCell = new PdfPCell(innerTable);
+        parentCell.setBorderWidth(2);
+
+        PdfPCell titleCell = new PdfPCell(new Phrase("Инвентарный номер", mainFont));
+        titleCell.setPaddingTop(10);
+        titleCell.setPaddingBottom(12);
+        titleCell.setHorizontalAlignment(Element.ALIGN_CENTER);
+
+        PdfPCell numberCell = new PdfPCell(new Phrase(device.getNumber(), mainFont));
+        numberCell.setPaddingTop(10);
+        numberCell.setPaddingBottom(12);
+        numberCell.setHorizontalAlignment(Element.ALIGN_CENTER);
+        numberCell.setBorderWidth(1);
+        numberCell.setBorderWidthBottom(2);
+
+        PdfPCell descriptionCell = new PdfPCell(new Phrase(device.getDescription(), descriptionFont));
+        descriptionCell.setPaddingBottom(3);
+
+        innerTable.addCell(titleCell);
+        innerTable.addCell(numberCell);
+        innerTable.addCell(descriptionCell);
+
+        table.addCell(parentCell);
     }
 }
